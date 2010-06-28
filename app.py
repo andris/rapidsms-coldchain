a4895810c6400c64241d3f6f2be11deb72368196
Andris Bjornson <andris@inveneo.org>
2010-06-28 10:03:39 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,234 @@
+import rapidsms
+from rapidsms.parsers.keyworder import *
+
+import re
+from models import *
+from datetime import datetime
+
+class App (rapidsms.app.App):
+
+    #Using the keyworder parser
+    kw = Keyworder()
+
+    #SmartConnect devices start messages with @ and end with !
+    sc_pat = re.compile(r'@(.*)!', re.IGNORECASE)
+
+    #compiled regex for reports
+    rpt_pat = re.compile(r'([A-Z]{3}),(\d+)', re.IGNORECASE)
+    
+    #compiled regex for alerts
+    alt_pat = re.compile(r'([A-Z]{3}),(0|1),(\d+),(\d+),(\d+)', re.IGNORECASE)
+
+    #compiled regex for config strings
+    cfg_pat = re.compile(r'(0|1),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)', re.IGNORECASE)
+    
+    def start (self):
+        """Configure your app in the start phase."""
+        pass
+
+    def parse (self, message):
+        """Parse and annotate messages in the parse phase."""
+        pass
+
+    def handle (self, message):
+        self.handled = False
+        try:
+            #Search for the @xxxx! SmartConnect syntax
+            sc_text = self.sc_pat.match(message.text)
+            if(sc_text):
+                #If we matched @xxxx! strip off @ and !
+                sc_command = sc_text.group(1)
+                if hasattr(self, "kw"):
+                    self.debug("HANDLE")
+                    
+                    # attempt to match tokens in the SC command
+                    # using the keyworder parser
+                    results = self.kw.match(self, sc_command)
+                    if results:
+                        func, captures = results
+                        # if a function was returned, then a this message
+                        # matches the handler _func_. call it, and short-
+                        # circuit further handler calls
+                        func(self, message, *captures)
+                        return self.handled
+                    else:
+                        self.debug("No SmartConnect keyword in %s" % message.text)
+                else:
+                    self.debug("App does not instantiate Keyworder as 'kw'")
+            else:
+                self.debug("No SmartConnect command in %s" % message.text)
+                
+        except Exception, e:
+            self.log_last_exception()
+
+    def cleanup (self, message):
+        """Perform any clean up after all handlers have run in the
+           cleanup phase."""
+        pass
+
+    def outgoing (self, message):
+        """Handle outgoing message notifications."""
+        pass
+
+    def stop (self):
+        """Perform global app cleanup when the application is stopped."""
+        pass
+
+    kw.prefix = "CFG"
+    @kw("(whatever)")
+    def receive_config(self, message, contents):
+        response = self.cfg_pat.match(contents)
+        if response:
+            report_is_configured = bool(int(response.group(1))==1)
+            report_low_thresh = int(response.group(2))
+            report_high_thresh = int(response.group(3))
+            report_rpt_freq = int(response.group(4))
+            report_alt_freq = int(response.group(5))
+            report_timeout = int(response.group(6))
+            report_imei = int(response.group(7))
+            report_fw = response.group(8)
+
+            self.debug("CFG string recieved,"
+                + " is_configured " + str(report_is_configured)
+                + " low thresh " + str(report_low_thresh)
+                + " high thresh " + str(report_high_thresh)
+                + " rpt freq " + str(report_rpt_freq)
+                + " alt freq " + str(report_alt_freq)
+                + " timeout " + str(report_timeout)
+                + " IMEI " + str(report_imei)
+                + " FW Ver " + report_fw)
+            
+            #Check if this SmartConnect device exists
+            if((len(SmartConnectClient.objects.filter(alias=report_imei))) > 0):
+                self.debug("CFG: Received CFG for existing client")
+            
+            #We've never seen this IMEI, register the
+            #new device    
+            else:
+                reporter = SmartConnectClient(
+                    first_name="SmartConnect",
+                    last_name=("IMEI:" + str(report_imei)),
+                    alias=report_imei,
+                    is_configured=report_is_configured,
+                    low_thresh=report_low_thresh,
+                    high_thresh=report_high_thresh,
+                    report_freq=report_rpt_freq,
+                    alert_freq=report_alt_freq,
+                    timeout=report_timeout,
+                    fw_version=report_fw)
+                    
+                reporter.save()
+                
+                #attach our new SmartConnect Device
+                #to the persistant connection
+                message.persistant_connection.reporter=reporter
+                message.persistant_connection.save()
+                
+                self.debug("Successfully registered new SmartConnect Device")
+
+        else:
+            self.debug("NO MATCHES IN CFG STRING")
+
+        self.handled = True
+
+    kw.prefix = "RPT"
+    @kw("(whatever)")
+    def receive_report(self, message, contents):
+        response = self.rpt_pat.match(contents)
+        if response:
+            report_type = response.group(1)
+            report_value = int(response.group(2))
+            #Need to update to take message sent time
+            report_time = datetime.now()
+            report_is_acknowledged = False
+
+            if message.reporter:
+                smart_connect_device = SmartConnectClient.objects.get(alias=message.reporter.alias)
+            
+            else:
+                self.debug("RECEIVED REPORT FROM UNREGISTERED DEVICE")
+                self.handled = False
+                return self.handled
+
+            report = SmartConnectReport(
+                reporting_device=smart_connect_device,
+                connection=message.persistant_connection,
+                type=report_type,
+                value=report_value,
+                time=report_time,
+                is_alert=False,
+                is_acknowledged=report_is_acknowledged,
+                floor=report_value,
+                ceiling=report_value)
+                
+            report.save()
+            self.debug("RPT received--" + str(report))
+
+        else:
+            self.debug("NO MATCHES IN RPT STRING")
+
+        self.handled = True
+
+    kw.prefix = "ALT"
+    @kw("(whatever)")
+    def receive_alert(self, message, contents):
+        response = self.alt_pat.match(contents)
+        if response:
+            alert_type = response.group(1)
+            alert_is_acknowledged = bool(int(response.group(2))==1)
+            alert_ceiling = int(response.group(3))
+            alert_floor = int(response.group(4))
+            alert_value = int(response.group(5))
+           #Need to update to take message sent time
+            alert_time = datetime.now()
+
+            if message.reporter:
+                smart_connect_device = SmartConnectClient.objects.get(alias=message.reporter.alias)
+
+            else:
+                self.debug("RECEIVED ALERT FROM UNREGISTERED DEVICE")
+                self.handled = False
+                return self.handled
+
+            report = SmartConnectReport(
+                reporting_device=smart_connect_device,
+                connection=message.persistant_connection,
+                type=alert_type,
+                value=alert_value,
+                time=alert_time,
+                is_alert=True,
+                is_acknowledged=alert_is_acknowledged,
+                floor=alert_floor,
+                ceiling=alert_ceiling)
+                
+            report.save()
+            self.debug("ALT received--" + str(report))
+
+        else:
+            self.debug("NO MATCHES IN ALT STRING")
+            
+        self.handled = True
+        
+    kw.prefix = "ACK"
+    @kw("(whatever)")
+    def receive_ack(self, message, contents):
+        self.debug("Got ACK with contents %s" % contents)
+        self.handled = True
+
+    kw.prefix = "ERR"
+    @kw("(whatever)")
+    def receive_error(self, message, contents):
+        self.debug("Got ERR with contents %s" % contents)
+        self.handled = True
+
+    kw.prefix = "VER"
+    @kw("(whatever)")
+    def receive_version(self, message, contents):
+        self.debug("Got VER with contents %s" % contents)
+        self.handled = True
+
+    kw.prefix = "MSG"
+    @kw("(whatever)")
+    def receive_version(self, message, contents):
+        self.debug("Got MSG with contents %s" % contents)
+        self.handled = True
---
+++ models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from reporters.models import Location, Reporter, PersistantConnection
+
+#Representation of a SmartConnect sensor (extends reporter)
+class SmartConnectClient(Reporter):
+    #The sensor reports if it has ever been configured by us
+    is_configured = models.BooleanField(default=False)
+
+    #lowest temperature can drop before alert (in K)
+    low_thresh = models.PositiveIntegerField(null=False, default=0)
+	
+    #highest temperature can go before alert (in K)
+    high_thresh = models.PositiveIntegerField(null=False, default=0)
+	
+    #how often to send standard temperature updates (in minutes)
+    report_freq = models.PositiveIntegerField(null=False, default=0)
+	
+    #how often to send updates when in alert state (in minutes)
+    alert_freq = models.PositiveIntegerField(null=False, default=0)
+	
+    #timeout.  Unused.  (in milleseconds)
+    timeout = models.PositiveIntegerField(null=False, default=0)
+	
+    #firmware version of smart connect device
+    fw_version = models.CharField(null=False, max_length=20)
+	
+    def __unicode__(self):
+        if self.connection():
+            return self.connection().identity
+        return self.alias
+
+class SmartConnectReport(models.Model):
+    #The SC device that sent this report
+    reporting_device = models.ForeignKey(SmartConnectClient, null=True, blank=True)
+	
+    #The Connection this report came in on
+    connection = models.ForeignKey(PersistantConnection, null=True, blank=True)
+	
+    #Three letter code for report type...currently using TMP for temperature
+    type = models.CharField(max_length=3)
+	
+    #The reported temperature (in K)
+    value = models.PositiveIntegerField(null=False, default=0)
+    
+    #The time of report (this should eventually be the message _sent_ time)
+    time = models.DateTimeField()
+    
+    #Was this flagged as an alert by the reporting device?
+    is_alert = models.BooleanField(default=False)
+
+    #Have we acknowledged this event / alert yet?
+    is_acknowledged = models.BooleanField(default=False)
+    
+    #These only apply for alerts not regular reports
+    ceiling = models.PositiveIntegerField(null=False, default=0)
+    floor = models.PositiveIntegerField(null=False, default=0)
+
+    def __unicode__(self):
+        string = ("SmartConnect Event,"
+            + " time: " + self.time.ctime()
+            + " type: " + str(self.type)
+            + " value: " + str(self.value)
+            + " is_alert " + str(self.is_alert)
+            + " is_acknowledged " + str(self.is_acknowledged)
+            + " floor " + str(self.floor)
+            + " ceiling " + str(self.ceiling))
+            
+        return string
